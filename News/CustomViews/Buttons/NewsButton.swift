6d291f00307f07317d95664a01b3b263daac2760
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-03-31 19:53:17 +0300

    Created Buttons folder in CustomViews folder. Created NewsButton in Buttons folder.

---
+++ News/CustomViews/Buttons/NewsButton.swift
@@ -0,0 +1,40 @@
+//
+//  NewsButton.swift
+//  News
+//
+//  Created by Владимир Мелещук on 31.03.2022.
+//
+
+import UIKit
+
+class NewsButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // Задаем кастомные тайтл и цвет кнопки.
+    convenience init(color: UIColor, title: String) {
+        self.init(frame: .zero)
+        set(color: color, title: title)
+    }
+
+    // Задаем постояные свойства кнопки: внешний вид (configuration) и cornerStyle.
+    private func configure() {
+        configuration = .tinted()
+        configuration?.cornerStyle = .medium
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    // Задаем все кастомные настройки.
+    func set(color: UIColor, title: String) {
+        configuration?.baseBackgroundColor = color
+        configuration?.baseForegroundColor = color
+        configuration?.title = title
+    }
+}
