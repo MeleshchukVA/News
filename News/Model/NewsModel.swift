1954a75df14c98e7d881c4f54845863f1381a6e3
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-03-31 19:16:46 +0300

    Created Model folder. Created NewsModel in Model folder.

---
+++ News/Model/NewsModel.swift
@@ -0,0 +1,22 @@
+//
+//  NewsModel.swift
+//  News
+//
+//  Created by Владимир Мелещук on 31.03.2022.
+//
+
+import Foundation
+
+struct News: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable, Hashable {
+    var author: String?
+    var title: String
+    var description: String?
+    var url: String?
+    var urlToImage: String?
+    var publishedAt: String?
+    var content: String?
+}
