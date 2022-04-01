6d91e8badee4efef4d01f99febcb9970adfba5ed
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-04-01 18:17:50 +0300

    Created Constants in Utilities folder. Added enum Images.

---
+++ News/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  News
+//
+//  Created by Владимир Мелещук on 01.04.2022.
+//
+
+import UIKit
+
+enum Images {
+    static let placeholder = UIImage(named: "placeholder-image")
+}
